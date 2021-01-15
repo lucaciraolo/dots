7e7ffa863fcb5fe57bda8c3adf663b5cc2923d2b
Luca Ciraolo <lucaciraolo@gmail.com>
2021-01-15 18:20:47 +0000

    switched names

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import SaveIcon from "@material-ui/icons/Save";
 import ClearIcon from "@material-ui/icons/Clear";
 
 export default function App() {
-  const players = ["Luca", "Cyrus", "Ravi", "Henry"];
+  const players = ["Pierrik", "Hanne", "Bjorn", "Finn"];
   const [scores, setScores] = useState([]);
 
   const [newScores, setNewScores] = useState(players.map((name) => null));
