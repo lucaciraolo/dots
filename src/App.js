f87579d572f0e9cff35cdd51ac78ffe87d8f8b73
Luca Ciraolo <lucaciraolo@gmail.com>
2022-01-07 15:13:36 +0000

    sizing

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import { ConfirmDialog } from "./ConfirmDialog";
 export default function App() {
   const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
   const [players, setPlayers] = useState(
-    ["Hugo", "Luca", "Cyrus", "Henry", "Henry", "Henry"],
+    ["Hugo", "Luca", "Cyrus", "Henry"],
     "players"
   );
   const [rounds, setRounds] = useStickyState([], "rounds");
@@ -51,99 +51,96 @@ export default function App() {
     return () => {};
   }, [rounds]);
 
-  // const tableCellPadding = "none";
-  const tableCellPadding = "normal";
+  const tableCellPadding = players.length > 4 ? "none" : "normal";
+  const maxWidth = players.length > 6 ? "sm" : "xs";
 
   return (
     <>
-      <Container>
-        <Box style={{ maxWidth: "100%", overflowX: "auto" }}>
-          <TableContainer>
-            <Table
-              aria-label="simple table"
-              size="small"
-              style={{ align: "center" }}
-              padding={tableCellPadding}
-            >
-              <TableHead>
-                <TableRow>
-                  {players.map((name) => (
-                    <TableCell align="center">{name}</TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rounds.map((round, roundNumber) => {
-                  const worstScore = Math.max(...round);
-                  return (
-                    <TableRow key={roundNumber}>
-                      {round.map((score) => {
-                        const style = {};
-                        if (score === worstScore) {
-                          style["color"] = "red";
-                        }
-                        return (
-                          <TableCell align="center" style={style}>
-                            {score}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-
-                {scoreEntry && (
-                  <TableRow>
-                    {newScores.map((score, index) => (
-                      <TableCell align="center">
-                        <TextField
-                          variant="standard"
-                          className={`score-input-${index}`}
-                          style={{ maxWidth: "3rem" }}
-                          value={score}
-                          type="number"
-                          onChange={(e) => {
-                            const newNewScores = [...newScores];
-                            newNewScores[index] = parseInt(e.target.value);
-                            setNewScores(newNewScores);
-                          }}
-                          onKeyPress={(event) => {
-                            if (event.key === "Enter") {
-                              if (index !== players.length - 1) {
-                                document
-                                  .querySelector(
-                                    `.score-input-${index + 1} input`
-                                  )
-                                  .focus();
-                              }
-                            }
-                          }}
-                          inputProps={{
-                            style: {
-                              appearance: "textfield",
-                            },
-                          }}
-                          {...(index === 0 && { autoFocus: true })}
-                        />
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                )}
-                {rounds.length > 0 && (
-                  <TableRow
-                    style={{ borderTop: "3px double rgba(81, 81, 81, 1)" }}
-                  >
-                    {totals.map((score) => (
-                      <TableCell align="center" style={{ fontWeight: "bold" }}>
+      <Container
+        style={{ height: "80vh", overflowX: "auto" }}
+        maxWidth={maxWidth}
+      >
+        {/* <TableContainer> */}
+        <Table
+          aria-label="simple table"
+          size="small"
+          style={{ align: "center" }}
+          padding={tableCellPadding}
+        >
+          <TableHead>
+            <TableRow>
+              {players.map((name) => (
+                <TableCell align="center">{name}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rounds.map((round, roundNumber) => {
+              const worstScore = Math.max(...round);
+              return (
+                <TableRow key={roundNumber}>
+                  {round.map((score) => {
+                    const style = {};
+                    if (score === worstScore) {
+                      style["color"] = "red";
+                    }
+                    return (
+                      <TableCell align="center" style={style}>
                         {score}
                       </TableCell>
-                    ))}
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+
+            {scoreEntry && (
+              <TableRow>
+                {newScores.map((score, index) => (
+                  <TableCell align="center">
+                    <TextField
+                      variant="standard"
+                      className={`score-input-${index}`}
+                      style={{ maxWidth: "3rem" }}
+                      value={score}
+                      type="number"
+                      onChange={(e) => {
+                        const newNewScores = [...newScores];
+                        newNewScores[index] = parseInt(e.target.value);
+                        setNewScores(newNewScores);
+                      }}
+                      onKeyPress={(event) => {
+                        if (event.key === "Enter") {
+                          if (index !== players.length - 1) {
+                            document
+                              .querySelector(`.score-input-${index + 1} input`)
+                              .focus();
+                          }
+                        }
+                      }}
+                      inputProps={{
+                        style: {
+                          appearance: "textfield",
+                        },
+                      }}
+                      {...(index === 0 && { autoFocus: true })}
+                    />
+                  </TableCell>
+                ))}
+              </TableRow>
+            )}
+            {rounds.length > 0 && (
+              <TableRow style={{ borderTop: "3px double rgba(81, 81, 81, 1)" }}>
+                {totals.map((score) => (
+                  <TableCell align="center" style={{ fontWeight: "bold" }}>
+                    {score}
+                  </TableCell>
+                ))}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        {/* </TableContainer> */}
       </Container>
 
       <Grid
